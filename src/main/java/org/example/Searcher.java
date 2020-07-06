8cf738119bd1dfb42f5ae00935fe15dfe78dfc0c
Elor170 <elor170@gmail.com>
2020-07-06 23:49:30 +0300

    Add weights to the fields, and reorganize Searcher.

--- src/main/java/org/example/Searcher.java
+++ src/main/java/org/example/Searcher.java
@@ -8,10 +8,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -19,37 +16,70 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Searcher {
+    //TODO find the optimal weights
+    static final float TITLE_WEIGHT = 0.2f;
+    static final float CAPTION_WEIGHT = (1 - TITLE_WEIGHT) / 3;
+    static final float HEADER_WEIGHT = (1 - TITLE_WEIGHT) / 3;
+    static final float COLUMN_WEIGHT = (1 - TITLE_WEIGHT) / 3;
 
-    public static void main(String[] args) throws IOException, ParseException {
+
+    public static void main(String[] args) throws IOException{
         LocalDateTime start = LocalDateTime.now();
 
         Analyzer enAnalyzer = new EnglishAnalyzer();
-        Directory index = FSDirectory.open(Paths.get("src\\main\\resources\\index"));
+        Directory indexDirectory = FSDirectory.open(Paths.get("src\\main\\resources\\index"));
+        IndexReader indexReader = DirectoryReader.open(indexDirectory);
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
-        String querystr = "phases of the moon";
-        //QueryParser queryParser = new QueryParser("caption", enAnalyzer);
-        QueryParser queryParser = new MultiFieldQueryParser(new String[]{"title", "caption", "header", "column"}
-        , enAnalyzer);
-        Query query = queryParser.parse(querystr);
-
-        int hitsPerPage = 25;
-        IndexReader reader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(query, hitsPerPage);
-        ScoreDoc[] hits = docs.scoreDocs;
-
-        System.out.println("Found " + hits.length + " hits.");
-        for(int i=0;i<hits.length;++i) {
-            int docId = hits[i].doc;
-            Document document = searcher.doc(docId);
-            System.out.println(document.get("id"));
+        try {
+            searchQueries(indexSearcher, enAnalyzer);
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+
+        indexDirectory.close();
+        indexReader.close();
 
         LocalDateTime end = LocalDateTime.now();
         Duration timeElapsed = Duration.between(start, end);
         System.out.println("Time taken: "+ timeElapsed.toMinutes() +" minutes " +
                 timeElapsed.toSeconds() +" seconds");
+    }
+
+    private static void searchQueries(IndexSearcher indexSearcher, Analyzer analyzer) throws ParseException, IOException {
+        // TODO add loop over the queries, and check the problem with table IDs...
+
+        String queryString = "world interest rates table";
+        String[] fieldsNames = new String[]{"title", "caption", "header", "column"};
+        Map<String,Float> fieldsWeights = new HashMap<>();
+        fieldsWeights.put("title", TITLE_WEIGHT);
+        fieldsWeights.put("caption", CAPTION_WEIGHT);
+        fieldsWeights.put("header", HEADER_WEIGHT);
+        fieldsWeights.put("column", COLUMN_WEIGHT);
+
+        QueryParser queryParser = new MultiFieldQueryParser(fieldsNames, analyzer, fieldsWeights);
+        Query query = queryParser.parse(queryString);
+
+        int hitsPerPage = 20;
+
+        TopDocs topDocs = indexSearcher.search(query, hitsPerPage);
+        ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+
+        System.out.println("Found " + scoreDocs.length + " hits:");
+        for (ScoreDoc scoreDoc: scoreDocs){
+            int docNum = scoreDoc.doc;
+            String docExplanation = indexSearcher.explain(query, docNum).toString();
+            String docScoring = docExplanation.substring(0, docExplanation.indexOf(" "));
+            Document document = indexSearcher.doc(docNum);
+            System.out.print("<query id>" + "   ");
+            System.out.print("Q0" + "   ");
+            System.out.print("table-" + document.get("id") + "    ");
+            System.out.print(docScoring + "   ");
+            System.out.println("Elor_Lior");
+        }
     }
 }
